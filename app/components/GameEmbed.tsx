cb793f71e691198be0373bde7cc49693bb227625
Lucas Dickey <lucas@lucasdickey.com>
2025-05-18 13:56:18 -0700

    Fix React import and ErrorBoundary implementation

--- app/components/GameEmbed.tsx
+++ app/components/GameEmbed.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, ErrorBoundary } from "react";
 
 export default function GameEmbed() {
   const [gameUrl, setGameUrl] = useState("");
