387a13cc088e31ef82c26be8ff7f9f49eec8ed61
Lucas Dickey <lucas@lucasdickey.com>
2025-05-30 16:38:12 -0700

    updated the footer stuff with new URLs

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import Link from "next/link";
+import { FaYoutube, FaSpotify, FaGithub } from 'react-icons/fa';
 
 export default function Footer() {
   return (
     <footer className="border-t border-secondary bg-light">
       <div className="container py-8 md:py-12">
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
           <div>
             <h3 className="text-lg font-bold">A-OK Store</h3>
             <p className="mt-2 text-sm">Nerd streetwear for AI junkies</p>
@@ -48,13 +49,36 @@ export default function Footer() {
                   Find us on X
                 </Link>
               </li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="text-lg font-bold">Follow Us</h3>
+            <ul className="mt-2 space-y-2">
+              <li>
+                <Link
+                  href="https://www.youtube.com/@apesonkeys/videos"
+                  target="_blank"
+                  className="text-sm hover:text-primary flex items-center"
+                >
+                  <FaYoutube className="mr-2 h-5 w-5" /> APES ON KNOWLEDGE
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://open.spotify.com/show/5mzflcTu9vWhB0Nw0lABRo"
+                  target="_blank"
+                  className="text-sm hover:text-primary flex items-center"
+                >
+                  <FaSpotify className="mr-2 h-5 w-5" /> Key To Sleep
+                </Link>
+              </li>
               <li>
                 <Link
                   href="https://github.com/lucasdickey/a-ok-shop"
                   target="_blank"
-                  className="text-sm hover:text-primary"
+                  className="text-sm hover:text-primary flex items-center"
                 >
-                  Go ape our shit
+                  <FaGithub className="mr-2 h-5 w-5" /> Go ape our shit
                 </Link>
               </li>
             </ul>
